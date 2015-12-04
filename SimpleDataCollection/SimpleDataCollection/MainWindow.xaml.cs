767e964fcac6556b08a798af3620b58f98a19d64
Paul Taele <ptaele@gmail.com>
2015-12-04 14:51:40 -0600

    added SimpleDataCollection project

---
+++ SimpleDataCollection/SimpleDataCollection/MainWindow.xaml.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Input;
+using Srl.Xml;
+
+namespace SimpleDataCollection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor and Loader
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowState = WindowState.Maximized;
+            WindowStyle = WindowStyle.ThreeDBorderWindow;
+            ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void MyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // initialize an empty list of strokes
+            myStrokes = new StrokeCollection();
+
+            // set the stylus and mouse state flags
+            IsStylusMove = false;
+            IsStylusEnd = false;
+            IsMouseDown = false;
+            IsMouseMove = false;
+            IsMouseUp = false;
+            IsMouseReady = false;
+
+            // add mouse button event handlers to the canvas for mouse up, mouse down, and mouse move
+            MyCanvas.AddHandler(InkCanvas.MouseDownEvent, new MouseButtonEventHandler(MyCanvas_PreviewMouseDown), true);
+            MyCanvas.AddHandler(InkCanvas.MouseDownEvent, new MouseButtonEventHandler(MyCanvas_PreviewMouseMove), true);
+            MyCanvas.AddHandler(InkCanvas.MouseDownEvent, new MouseButtonEventHandler(MyCanvas_PreviewMouseUp), true);
+        }
+
+        #endregion
+
+        #region Mouse and Stylus Events
+
+        private void MyCanvas_StylusButtonDown(object sender, StylusButtonEventArgs e)
+        {
+            // update the stylus interaction flags
+            IsStylusMove = true;
+
+            // initialize the points and times
+            myPoints = new StylusPointCollection();
+            myTimes = new List<int>();
+            myTimeOffset = 0;
+
+            // update the stroke with the initial x, y, and time
+            UpdateStroke(
+                e.GetPosition(MyCanvas).X,
+                e.GetPosition(MyCanvas).Y,
+                myPoints,
+                myTimes,
+                true);
+
+            // enable interaction buttons
+            EnableButtons(true);
+        }
+
+        private void MyCanvas_StylusMove(object sender, StylusEventArgs e)
+        {
+            // update the stroke with the current x, y, and time
+            UpdateStroke(
+                e.GetPosition(MyCanvas).X,
+                e.GetPosition(MyCanvas).Y,
+                myPoints,
+                myTimes,
+                false);
+        }
+
+        private void MyCanvas_StylusButtonUp(object sender, StylusButtonEventArgs e)
+        {
+            // update the stylus interaction flags
+            IsStylusMove = false;
+            IsStylusEnd = true;
+
+            // update the stroke with the final x, y, and time
+            UpdateStroke(
+                e.GetPosition(MyCanvas).X,
+                e.GetPosition(MyCanvas).Y,
+                myPoints,
+                myTimes,
+                false);
+
+            // add stroke to the list
+            Stroke stroke = new Stroke(myPoints);
+            stroke.AddPropertyData(SketchXmlProcessor.TIMES_GUID, myTimes.ToArray());
+            myStrokes.Add(stroke);
+        }
+
+        private void MyCanvas_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // update the stylus interaction flags
+            IsMouseDown = !IsMouseDown;
+            IsMouseMove = false;
+            if (!IsMouseDown) { return; }
+            if (IsStylusMove) { return; }
+
+            // initialize the points and times
+            myPoints = new StylusPointCollection();
+            myTimes = new List<int>();
+            myTimeOffset = 0;
+
+            // update the stroke with the initial x, y, and time
+            UpdateStroke(
+                e.GetPosition(MyCanvas).X,
+                e.GetPosition(MyCanvas).Y,
+                myPoints,
+                myTimes,
+                true);
+
+            // enable interaction buttons
+            EnableButtons(true);
+        }
+
+        private void MyCanvas_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            // update the stylus interaction flags
+            if (myPoints == null) { return; }
+            if (e.LeftButton != MouseButtonState.Pressed) { return; }
+            if (IsMouseDown) { return; }
+            if (!IsMouseMove) { IsMouseMove = true; return; }
+            if (!IsMouseReady) { IsMouseReady = true; return; }
+            if (IsStylusMove) { return; }
+
+            // update the stroke with the current x, y, and time
+            UpdateStroke(
+                e.GetPosition(MyCanvas).X,
+                e.GetPosition(MyCanvas).Y,
+                myPoints,
+                myTimes,
+                false);
+        }
+
+        private void MyCanvas_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            // update the stylus interaction flags
+            if (myPoints == null) { return; }
+            IsMouseUp = !IsMouseUp;
+            if (IsMouseUp) { return; }
+            if (IsStylusMove) { return; }
+            if (IsStylusEnd) { IsStylusEnd = false; return; }
+
+            // update the stroke with the final x, y, and time
+            UpdateStroke(
+                e.GetPosition(MyCanvas).X,
+                e.GetPosition(MyCanvas).Y,
+                myPoints,
+                myTimes,
+                false);
+
+            // add the stroke to the list
+            Stroke stroke = new Stroke(myPoints);
+            stroke.AddPropertyData(SketchXmlProcessor.TIMES_GUID, myTimes.ToArray());
+            myStrokes.Add(stroke);
+        }
+
+        #endregion
+
+        #region Button Interactions
+
+        private void MySaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // set up the open file dialog
+            var dialog = new System.Windows.Forms.SaveFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            dialog.DefaultExt = "xml";
+            dialog.Filter = "XML files|*.xml|All Files (*.*)|*.*";
+            dialog.FilterIndex = 1;
+            dialog.RestoreDirectory = true;
+
+            // show the open file dialog
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // get the file path of the XML file
+                string filePath = dialog.FileName;
+                var processor = new SketchXmlProcessor();
+
+                // save the strokes to the file path
+                processor.Write(filePath, "Unlabeled", myStrokes);
+                //return;
+            }
+        }
+
+        private void MyLoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            // set up the open file dialog
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            dialog.DefaultExt = "xml";
+            dialog.Filter = "XML files|*.xml|All Files (*.*)|*.*";
+            dialog.FilterIndex = 1;
+            dialog.Multiselect = false;
+            dialog.RestoreDirectory = true;
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+
+            // show the open file dialog
+            if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+
+            // clear the strokes from the canvas
+            ClearStrokes();
+
+            // get the file path of the XML file
+            string filePath = dialog.FileName;
+            var processor = new SketchXmlProcessor();
+
+            // load and set the strokes
+            StrokeCollection strokes = processor.Read(filePath);
+            foreach (var stroke in strokes)
+            {
+                SetStrokeProperties(stroke);
+            }
+
+            // set the recorded and canvas strokes
+            myStrokes = strokes;
+            MyCanvas.Strokes.Add(myStrokes);
+
+            // enable interaction buttons
+            EnableButtons(true);
+        }
+
+        private void MyClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            // clear the strokes from the canvas
+            ClearStrokes();
+
+            // disable interaction buttons
+            EnableButtons(false);
+        }
+
+        private void MyUndoButton_Click(object sender, RoutedEventArgs e)
+        {
+            // case: there are strokes on the canvas
+            if (MyCanvas.Strokes.Count > 0)
+            {
+                // remove the most recent stroke added from the canvas
+                MyCanvas.Strokes.RemoveAt(MyCanvas.Strokes.Count - 1);
+
+                // remove the most recent stroke added from the list of strokes
+                myStrokes.RemoveAt(myStrokes.Count - 1);
+            }
+
+            // case: there are now no strokes on the canvas
+            if (MyCanvas.Strokes.Count == 0)
+            {
+                // disable interaction buttons
+                EnableButtons(false);
+            }
+        }
+
+        private void EnableButtons(bool enable)
+        {
+            MySaveButton.IsEnabled = enable;
+            MyClearButton.IsEnabled = enable;
+            MyUndoButton.IsEnabled = enable;
+        }
+
+        #endregion
+
+        #region Stroke Modifiers
+
+        private void UpdateStroke(double x, double y, StylusPointCollection points, List<int> times, bool isFirstPoint)
+        {
+            // get the time
+            long fullTime = DateTime.Now.Ticks;
+
+            // record the subtracting constant (to account for overflow when later converting to int)
+            if (isFirstPoint)
+            {
+                myTimeOffset = fullTime;
+            }
+
+            // subtract out the constant value
+            fullTime = fullTime - myTimeOffset;
+
+            // convert time to int
+            int time = (int)fullTime;
+
+            // add the point and time to their respective lists
+            points.Add(new StylusPoint(x, y));
+            times.Add(time);
+        }
+
+        private void ClearStrokes()
+        {
+            MyCanvas.Strokes.Clear();
+            myStrokes = new StrokeCollection();
+        }
+
+        private void SetStrokeProperties(Stroke stroke)
+        {
+            stroke.DrawingAttributes.Width = MyCanvas.DefaultDrawingAttributes.Width;
+            stroke.DrawingAttributes.Height = MyCanvas.DefaultDrawingAttributes.Height;
+            stroke.DrawingAttributes.Color = MyCanvas.DefaultDrawingAttributes.Color;
+        }
+
+        #endregion
+
+        #region Stylus and Mouse Flags
+
+        private bool IsStylusMove { get; set; }
+        private bool IsStylusEnd { get; set; }
+        private bool IsMouseDown { get; set; }
+        private bool IsMouseMove { get; set; }
+        private bool IsMouseUp { get; set; }
+        private bool IsMouseReady { get; set; }
+
+        #endregion
+
+        #region Fields
+
+        private StylusPointCollection myPoints;
+        private List<int> myTimes;
+        private StrokeCollection myStrokes;
+        private long myTimeOffset;
+
+        #endregion
+    }
+}
