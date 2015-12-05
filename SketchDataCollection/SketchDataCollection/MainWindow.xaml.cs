9065f313a451b649d991cd1f7d3c4238e7b883af
Paul Taele <ptaele@gmail.com>
2015-12-05 16:59:20 -0600

    added new SketchDataCollection project

---
+++ SketchDataCollection/SketchDataCollection/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SketchDataCollection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowState = WindowState.Maximized;
+            WindowStyle = WindowStyle.ThreeDBorderWindow;
+            ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void MyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
